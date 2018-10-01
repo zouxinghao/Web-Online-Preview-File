8ecf88e6458c6c21777c7dbe513acb9d09457fc0
Xinghao Zou <527309305@qq.com>
2018-09-30 18:35:40 -0700

    :package: defination types

---
+++ src/main/java/model/ReturnResponse.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.io.Serializable;
+
+public class ReturnResponse<T> implements Serializable {
+	private static final long serialVersionUID = 313975329998789878L;
+	
+	/**
+	 * return code
+	 * 0: success
+	 * 1: fail
+	 */
+	private int code;
+	
+	private String msg;
+	
+	private T content;
+	
+	public ReturnResponse(int code, String msg, T content) {
+        this.code = code;
+        this.msg = msg;
+        this.content = content;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+}
